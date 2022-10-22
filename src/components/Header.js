65ca82e699e5e17d6a1b8b43c67ab2dfada2fbb0
camilo <camiloacevedomoreno@gmail.com>
2022-10-22 15:53:52 -0500

    setup hello world in project

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/components/header.css";
+
+const Header = () => {
+    return (
+        <h1>Hello world</h1>
+    )
+}
+
+export default Header
---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/pages/app.css";
+import Header from "../components/Header"
+
+const App = () =>{
+    return(
+        <Header />
+    )
+}
+
+export default App
